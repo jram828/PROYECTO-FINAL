5455453d5f708041a92d38e363f894b8b78cecf9
jram828 <128335805+jram828@users.noreply.github.com>
2024-04-28 12:03:32 -0500

    CREACION PROYECTO

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+import './App.css'
+
+function App() {
+  
+  return (
+ console.log('Aun no hay nada')
+  )
+}
+
+export default App
---
+++ Client/src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { createRoot } from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <GoogleOAuthProvider clientId="844309234209-e072sos6qgabufkk0tmgv74900fknd62.apps.googleusercontent.com">
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </GoogleOAuthProvider>
+);
