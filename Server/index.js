dbb348e2ce7cb77d03280121c205aa52ff6dea2a
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-10 10:37:38 -0500

    CONFIG SERVER

---
+++ Server/index.js
@@ -0,0 +1,12 @@
+const server = require("./src/server");
+const { conn } = require('./src/db.js');
+ 
+const PORT = 3001;
+ 
+  conn.sync({ alter: true }).then(() => { 
+}).catch(error => console.error(error))
+    
+    server.listen(PORT, () => {
+      
+      console.log(`Server listening on port ${PORT}`);
+})
