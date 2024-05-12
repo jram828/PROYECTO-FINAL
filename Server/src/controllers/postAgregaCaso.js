af89d8e8893cc4606f2fb8021a34abe41a2dadc7
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-11 23:05:42 -0500

    fix Ruta Crear Caso

--- Server/src/controllers/postAgregaCaso.js
+++ Server/src/controllers/postAgregaCaso.js
@@ -3,8 +3,14 @@ const { Caso } = require("../db_conn");
 const createCaso = async (cedulaCliente,cedulaAbogado, fecha, descripcion,TipoDeCasoId) => {
     // console.log('imagen',imagen)
     console.log('Cliente....',cedulaCliente,'  Abogado..... ',cedulaAbogado)
-    const newCaso = await Caso.create({fecha: fecha, descripcion: descripcion,TipoDeCasoId: TipoDeCasoId,
-        cedulaCliente: cedulaCliente,cedulaAbogado: cedulaAbogado})
+    const newCaso = await Caso.create({
+      fecha: fecha,
+      descripcion: descripcion,
+      TipoDeCasoId: TipoDeCasoId,
+      cedulaCliente: cedulaCliente,
+      cedulaAbogado: cedulaAbogado,
+      fechaFin: fechaFin,
+    });
 
     //  newAbogado.addCliente(clientes);
    
