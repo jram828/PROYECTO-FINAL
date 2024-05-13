b6294913860d3f9521758be720cbf8600c35227b
melizulian <melizulian@hotmail.com>
2024-05-13 16:24:05 -0300

    diseno casos

--- Client/src/views/cases/cases.component.jsx
+++ Client/src/views/cases/cases.component.jsx
@@ -1,4 +1,4 @@
-import './cases.css';
+import style from './cases.module.css';
 import React from 'react';
 import Filtros from '../../components/filtros';
 import Order from '../../components/order';
@@ -6,15 +6,20 @@ import { Link } from 'react-router-dom';
 
 function Cases() {
   return (
-    <div>
-      <div>
-        <p>Casos</p>
-        <Order></Order>
-       <Filtros></Filtros>
+    <div className={style.container}>
+      
+      <div className={style.container2}>
+        <div className={style.ordercontainer}>
+          <Order></Order>
+        </div>
+        <div className={style.filtroscontainer}>
+          <Filtros></Filtros>
+        </div>
       </div>
-      <Link to='/home'>
-          <button >Volver</button>
-          </Link>
+        <Link to='/home'>
+        <button className='button'>Volver</button>
+        </Link>
+        <Link to='/home/cases/crearcaso'><button className='button'>Crear caso</button></Link>
     </div>
   )
 }
