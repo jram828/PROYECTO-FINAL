28ca71dae07be40e6388f9ae1a800366b3e0104c
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-17 14:10:31 -0500

    boton pagos

--- Server/src/controllers/pagos/crearOrden.js
+++ Server/src/controllers/pagos/crearOrden.js
@@ -1,8 +1,34 @@
-// const { MercadoPagoConfig, Payment } = require("mercadopago");
+const { MercadoPagoConfig, Payment, Preference } = require("mercadopago");
 
-const { mercadopago } = require("mercadopago");
+// const { mercadopago } = require("mercadopago");
 
 const crearOrden = async () => {
+  // SDK de Mercado Pago
+
+  // Agrega credenciales
+  const client = new MercadoPagoConfig({
+    accessToken:
+      "TEST-7845349164975835-051714-6919a564464a369582bcbc8ff6f8cc4f-1817941600",
+  });
+
+  
+const preference = new Preference(client);
+
+preference
+  .create({
+    body: {
+      items: [
+        {
+          title: "Honorarios",
+          quantity: 1,
+          unit_price: 200,
+        },
+      ],
+    },
+  })
+  .then(console.log)
+  .catch(console.log);
+
   // // Step 2: Initialize the client object
   // const client = new MercadoPagoConfig({
   //   accessToken:
@@ -30,21 +56,21 @@ const crearOrden = async () => {
 
   // // Step 6: Make the request
   // const newPayment=payment.create({ body, requestOptions }).then(console.log).catch(console.log);
-  mercadopago.configure({
-    access_token:
-      "TEST-3176577694700734-051711-d19831d5da8b20319a010655906a334c-1817941600",
-  });
+  // mercadopago.configure({
+  //   access_token:
+  //     "TEST-7845349164975835-051714-6919a564464a369582bcbc8ff6f8cc4f-1817941600",
+  // });
 
-  const newOrden = await mercadopago.preferences.create({
-    item: [
-      {
-        title: "honorarios",
-        unit_price: 20,
-        currency_id: "COP",
-        quantity: 1,
-      },
-    ],
-  });
+  // const newOrden = await mercadopago.preferences.create({
+  //   item: [
+  //     {
+  //       title: "honorarios",
+  //       unit_price: 20,
+  //       currency_id: "COP",
+  //       quantity: 1,
+  //     },
+  //   ],
+  // });
 
   console.log("result crear orden:", newOrden);
   return newOrden;
