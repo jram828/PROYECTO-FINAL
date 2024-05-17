996513d0e39d1cf8c4c9f8b1512b4b1fc5921fb4
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-17 11:15:02 -0500

    crear orden handler ruta pagos

--- Server/src/routes/paymentsRoutes.js
+++ Server/src/routes/paymentsRoutes.js
@@ -1,12 +1,12 @@
 const { Router } = require("express");
 const { getPaymentsHandler } = require("../handlers/paymentsHandler");
-// const { postPaymentsHandler } = require("../handlers/paymentsHandler");
+const { crearOrdenHandler } = require("../handlers/paymentsHandler");
 
 const paymentsRouter = Router();
 
 paymentsRouter.get("/", getPaymentsHandler);
 paymentsRouter.get("/crearorden", crearOrdenHandler);
-paymentsRouter.get("/success", successHandler);
-paymentsRouter.get("/webhook", webhookHandler);
+// paymentsRouter.get("/success", successHandler);
+// paymentsRouter.get("/webhook", webhookHandler);
 
 module.exports = {paymentsRouter};
