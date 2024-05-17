edd7078d2b49f94a110fb874fa3c62b13aac0f3f
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-17 11:19:36 -0500

    payments router

--- Server/src/routes/paymentsRoutes.js
+++ Server/src/routes/paymentsRoutes.js
@@ -9,4 +9,4 @@ paymentsRouter.get("/crearorden", crearOrdenHandler);
 // paymentsRouter.get("/success", successHandler);
 // paymentsRouter.get("/webhook", webhookHandler);
 
-module.exports = {paymentsRouter};
+module.exports = paymentsRouter;
