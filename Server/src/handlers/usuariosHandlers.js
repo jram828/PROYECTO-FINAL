4fdc1ecf794226eefb0686ade4e158f52a145918
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-20 19:54:27 -0500

    IMPLEMENTACION TWILIO

--- Server/src/handlers/usuariosHandlers.js
+++ Server/src/handlers/usuariosHandlers.js
@@ -1,5 +1,7 @@
 const { getAllUsuario } = require("../controllers/usuario/getAllUsuario");
 const { crearUsuario } = require("../controllers/usuario/insertaUsuario");
+const { accountSid, authToken, number } = process.env;
+const twilio = require("twilio");
 
 const allUsuarios = async (req, res)=>{
     //const { name } = req.query;
@@ -31,8 +33,20 @@ const postUsuariosHandler = async (req, res) => {
       imagen,
       rol
     );
-    if (response) res.status(200).json(response);
-    else res.status(200).send("El usuario ya existe");
+    if (response) {
+      res.status(200).json(response);
+      console.log("Datos Twilio:", { accountSid, authToken, number });
+
+      const client = new twilio(accountSid, authToken,number);
+      client.messages.create({
+        body: "Se ha creado un nuevo usuario en Legaltech!",
+        from: number,
+        to: "+573204746006",
+      });
+    }
+    
+    else { res.status(200).send("El usuario ya existe"); }
+
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
