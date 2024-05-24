75704d1016ab16f79089672c76ecd4375b19e28f
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-24 16:04:06 -0500

    fix obtener pago

--- Client/src/views/status/index.jsx
+++ Client/src/views/status/index.jsx
@@ -32,22 +32,23 @@ function Status () {
   console.log("objeto queries: ", queries);
   console.log("Payment id: ", queries.payment_id);
   
-  const obtenerPago = async(id) => {
+  const obtenerPago = async (id) => {
     console.log("Payment id: ", id);
-      try {
-    const { data } = await axios.get(
-      `https://api.mercadopago.com/v1/payments/${id}`,
-      {
-        headers: {
-          'Authorization':
-            "Bearer TEST-3176577694700734-051711-d19831d5da8b20319a010655906a334c-1817941600",
-        },
-      }
-    );
-    console.log("Data obtener pago: ", data);
-    return data
-  }catch (error) {
-    window.alert("No se obtuvieron los datos del pago");
+    try {
+      const { data } = await axios.get(
+        `https://api.mercadopago.com/v1/payments/${id}`,
+        {
+          headers: {
+            'Authorization':
+              "Bearer TEST-3176577694700734-051711-d19831d5da8b20319a010655906a334c-1817941600",
+          },
+        }
+      );
+      console.log("Data obtener pago: ", data);
+      return data
+    } catch (error) {
+      window.alert("No se obtuvieron los datos del pago");
+    }
   }
   
   const datosPago = obtenerPago(queries.payment_id);
