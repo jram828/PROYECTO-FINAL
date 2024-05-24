a8037b3975142eb93b0bfd1452e0934789a7aec1
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-23 20:20:28 -0500

    hash router

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,7 +1,7 @@
 import "./index.css"; 
 import './styles.css';
 import App from "./App";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import { createRoot } from "react-dom/client";
