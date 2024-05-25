bb2264a16c1983519661b597a326e8869a943e44
JDRengifo <josedariorf@gmail.com>
2024-05-25 09:11:39 -0300

    Tabla de pago de clientes

---
+++ Server/src/controllers/pagos/status.js
@@ -0,0 +1,59 @@
+const axios = require("axios");
+const {
+  createPagosClientes,
+} = require("../pagosClientesControllers/postPagosClientes");
+
+const obtenerPago = async (idPago) => {
+  // console.log("Payment id: ", idPago);
+  try {
+    const { data } = await axios.get(
+      `https://api.mercadopago.com/v1/payments/${idPago}`,
+      {
+        headers: {
+          Authorization:
+            "Bearer TEST-3176577694700734-051711-d19831d5da8b20319a010655906a334c-1817941600",
+        },
+      },
+    );
+
+    const idCaso = data.additional_info.items[0].id;
+    const orderId = data.order.id;
+
+    const {
+      description,
+      date_approved,
+      id,
+      payment_type_id,
+      status,
+      transaction_amount,
+    } = data;
+
+    // console.log("Data obtener pago: ", data);
+    // console.log("Data obtener pago2: ", {
+    //   date_approved,
+    //   description,
+    //   id,
+    //   payment_type_id,
+    //   status,
+    //   transaction_amount,
+    // });
+    const newPago = createPagosClientes(
+      idCaso,
+      description,
+      date_approved,
+      id,
+      orderId,
+      payment_type_id,
+      status,
+      transaction_amount,
+    );
+    return newPago;
+  } catch (error) {
+    console.log(error);
+    // window.alert("No se obtuvieron los datos del pago");
+  }
+};
+
+module.exports = {
+  obtenerPago,
+};
--- Server/src/handlers/paymentsHandler.js
+++ Server/src/handlers/paymentsHandler.js
@@ -1,24 +1,25 @@
-const { crearOrden } = require('../controllers/pagos/crearOrden')
-const { failure } = require('../controllers/pagos/failure')
-const { getPayments } = require('../controllers/pagos/getPayments')
-const { pending } = require('../controllers/pagos/pending')
-const { success } = require('../controllers/pagos/success')
-const { webhook } = require('../controllers/pagos/webhook')
+const { crearOrden } = require("../controllers/pagos/crearOrden");
+const { failure } = require("../controllers/pagos/failure");
+const { getPayments } = require("../controllers/pagos/getPayments");
+const { pending } = require("../controllers/pagos/pending");
+const { obtenerPago } = require("../controllers/pagos/status");
+const { webhook } = require("../controllers/pagos/webhook");
 
-const getPaymentsHandler = async (req, res)=>{
-    try {
-        const { password, email } = req.query
-        const response = await getPayments(password, email)
-        res.status(200).json(response)
-    } catch (error) {
-        res.status(400).json({error:error.message})
-    }
-}
-
-const successHandler = async (req, res) => {
+const getPaymentsHandler = async (req, res) => {
   try {
-    // const { password, email } = req.query;
-    const response = await success(req.body);
+    const { password, email } = req.query;
+    const response = await getPayments(password, email);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const statusHandler = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const response = await obtenerPago(id);
+    // console.log(response);
     res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -30,7 +31,7 @@ const webhookHandler = async (req, res) => {
     // const { password, email } = req.query;
     console.log("Webhook mercado pago: ", req.body);
     const response = await webhook(req.body);
-    
+
     res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -60,8 +61,8 @@ const pendingHandler = async (req, res) => {
 const crearOrdenHandler = async (req, res) => {
   try {
     // const { password, email } = req.query;
-      const response = await crearOrden(req.body);
-      console.log('response hander:',response)
+    const response = await crearOrden(req.body);
+    console.log("response hander:", response);
     res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -73,6 +74,6 @@ module.exports = {
   crearOrdenHandler,
   pendingHandler,
   failureHandler,
-  successHandler,
+  statusHandler,
   webhookHandler,
 };
