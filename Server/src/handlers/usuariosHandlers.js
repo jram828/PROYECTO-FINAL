39c8f517cdf7a14387546ff727af3786c902a496
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-25 06:16:28 -0500

    NOTIFICACION NODEMAILER

--- Server/src/handlers/usuariosHandlers.js
+++ Server/src/handlers/usuariosHandlers.js
@@ -3,21 +3,19 @@ const { crearUsuario } = require("../controllers/usuario/insertaUsuario");
 require("dotenv").config();
 const { ACCOUNTSID, AUTHTOKEN, NUMBER } = process.env;
 const twilio = require("twilio");
+const nodemailer = require("nodemailer");
+const fs = require("fs");
+const { GOOGLE_KEY } = process.env;
+require("dotenv").config();
 
 const allUsuarios = async (req, res)=>{
-    //const { name } = req.query;
+
     console.log(req.query)
-    //const { pagina=1, porPagina=10 }=req.query
-    //const offset=(parseInt(pagina) - 1) * parseInt(porPagina)
       
     try {
         
             const response = await getAllUsuario()
             res.status(200).json(response)
-        /*}  else {
-            const countyByName = await getClienteByName(name)
-            res.status(200).json(countyByName);
-        } ;*/
         } catch (error) {
             res.status(400).json({error: error.message})
     }
@@ -50,6 +48,31 @@ const postUsuariosHandler = async (req, res) => {
         })
         .then((message) => console.log(message.sid))
         .done();
+      
+       const transporter = nodemailer.createTransport({
+         host: "smtp.gmail.com",
+         port: 465,
+         secure: true,
+         auth: {
+           user: "legaltech@gmail.com",
+           pass: GOOGLE_KEY,
+         },
+         tls: {
+           rejectUnauthorized: false,
+         },
+       });
+       async function sendEmail(correo) {
+
+         const info = await transporter.sendMail({
+           from: '"Legaltech" <legaltech.crm@gmail.com>',
+           to: correo,
+           subject: `Hola!`,
+           text: "Has sido registrado en Legaltech!",
+         });
+
+         console.log("Message sent: %s", info.messageId);
+       }
+       sendEmail();
       
     }
     
--- Server/src/routes/paymentsRoutes.js
+++ Server/src/routes/paymentsRoutes.js
@@ -1,14 +1,13 @@
 const { Router } = require("express");
-const { getPaymentsHandler, failureHandler, pendingHandler, webhookHandler, statusHandler } = require("../handlers/paymentsHandler");
-const { crearOrdenHandler } = require("../handlers/paymentsHandler");
+
+const { checkoutHandler, getPaymentsHandler, statusHandler, webhookHandler } = require("../handlers/checkoutHandler");
 
 const paymentsRouter = Router();
 
 paymentsRouter.get("/", getPaymentsHandler);
-paymentsRouter.post("/crearorden", crearOrdenHandler);
+paymentsRouter.post("/checkout", checkoutHandler);
 paymentsRouter.post("/status", statusHandler);
 paymentsRouter.post("/webhook", webhookHandler);
-paymentsRouter.get("/failure", failureHandler);
-paymentsRouter.get("/pending", pendingHandler);
+
 
 module.exports = paymentsRouter;
