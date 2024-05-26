ce97e607c7e01d32517dd884f14deaf72e3cf2a6
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-26 12:10:09 -0500

    login propio buscar usuario

--- Server/src/controllers/usuario/login.js
+++ Server/src/controllers/usuario/login.js
@@ -1,31 +1,39 @@
-const { Cliente, Abogado, Usuario } = require('../../DB')
+const { Cliente, Abogado, Usuario } = require("../../DB");
 
-const getLogin = async (password, email)=>{
-    const login = await Cliente.findOne({
-        where:{
-            correo: email,
-            password: password
-        }
-    })
-    if(!login){
-        const login = await Abogado.findOne({
-            where:{
-                correo: email,
-                password: password
-            }
-        })
-        if(!login) throw new Error('Password o email inválido')
-        return{
-            access: true,
-            usuario: login
-        }
-    }
+const getLogin = async (password, email) => {
+  const login = await Cliente.findOne({
+    where: {
+      correo: email,
+      password: password,
+    },
+  });
+  if (!login) {
+    login = await Abogado.findOne({
+      where: {
+        correo: email,
+        password: password,
+      },
+    });
+  }
+  if (!login) {
+    const login = await Usuario.findOne({
+      where: {
+        correo: email,
+        password: password,
+      },
+    });
+    if (!login) throw new Error("Password o email inválido");
     return {
-        access: true,
-        usuario: login
-    }
-}
+      access: true,
+      usuario: login,
+    };
+  }
+  return {
+    access: true,
+    usuario: login,
+  };
+};
 
 module.exports = {
-    getLogin
-}
+  getLogin,
+};
