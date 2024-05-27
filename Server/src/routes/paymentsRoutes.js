fc7895bf81bba0d9edc8c1d7877aa4e0d0dab18d
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-26 19:07:27 -0500

    resolver conflictos payments routes

--- Server/src/routes/paymentsRoutes.js
+++ Server/src/routes/paymentsRoutes.js
@@ -1,10 +1,4 @@
 const { Router } = require("express");
-<<<<<<< HEAD
-const { getPaymentsHandler, successHandler, failureHandler, pendingHandler, webhookHandler } = require("../handlers/paymentsHandler");
-const { crearOrdenHandler } = require("../handlers/paymentsHandler");
-
-=======
-
 const {
   getPaymentsHandler,
   failureHandler,
@@ -16,20 +10,14 @@ const {
 const { crearOrdenHandler } = require("../handlers/paymentsHandler");
 
 
->>>>>>> 51d92237903119989773bfccf662e0381c69085c
+
 const paymentsRouter = Router();
 
 paymentsRouter.get("/", getPaymentsHandler);
 paymentsRouter.post("/crearorden", crearOrdenHandler);
-<<<<<<< HEAD
-paymentsRouter.get("/success", successHandler);
+paymentsRouter.get("/staus", statusHandler);
 paymentsRouter.post("/webhook", webhookHandler);
 paymentsRouter.get("/failure", failureHandler);
 paymentsRouter.get("/pending", pendingHandler);
-=======
-paymentsRouter.post("/status", statusHandler);
-paymentsRouter.post("/webhook", webhookHandler);
-
->>>>>>> 51d92237903119989773bfccf662e0381c69085c
 
 module.exports = paymentsRouter;
