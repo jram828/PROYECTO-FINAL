e9d87ae75a15d6a39af413c2b206bd5084b32db2
JONAAVILA <jona_1969@hotmail.com>
2024-05-26 22:14:37 -0300

    update

--- Server/src/handlers/paymentsHandler.js
+++ Server/src/handlers/paymentsHandler.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-const { crearOrden } = require('../controllers/pagos/crearOrden')
-const { failure } = require('../controllers/pagos/failure')
-const { getPayments } = require('../controllers/pagos/getPayments')
-const { pending } = require('../controllers/pagos/pending')
-const { success } = require('../controllers/pagos/success')
-const { webhook } = require('../controllers/pagos/webhook')
-
-const getPaymentsHandler = async (req, res)=>{
-    try {
-        const { password, email } = req.query
-        const response = await getPayments(password, email)
-        res.status(200).json(response)
-    } catch (error) {
-        res.status(400).json({error:error.message})
-    }
-}
-
-const successHandler = async (req, res) => {
-  try {
-    // const { password, email } = req.query;
-    const response = await success(req.body);
-=======
 const { crearOrden } = require("../controllers/pagos/crearOrden");
 const { failure } = require("../controllers/pagos/failure");
 const { getPayments } = require("../controllers/pagos/getPayments");
@@ -45,7 +22,6 @@ const statusHandler = async (req, res) => {
 
     // console.log(response);
 
->>>>>>> 51d92237903119989773bfccf662e0381c69085c
     res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -57,11 +33,6 @@ const webhookHandler = async (req, res) => {
     // const { password, email } = req.query;
     console.log("Webhook mercado pago: ", req.body);
     const response = await webhook(req.body);
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> 51d92237903119989773bfccf662e0381c69085c
     res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -91,13 +62,8 @@ const pendingHandler = async (req, res) => {
 const crearOrdenHandler = async (req, res) => {
   try {
     // const { password, email } = req.query;
-<<<<<<< HEAD
-      const response = await crearOrden(req.body);
-      console.log('response hander:',response)
-=======
     const response = await crearOrden(req.body);
     console.log("response hander:", response);
->>>>>>> 51d92237903119989773bfccf662e0381c69085c
     res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -108,13 +74,6 @@ module.exports = {
   getPaymentsHandler,
   crearOrdenHandler,
   pendingHandler,
-<<<<<<< HEAD
-  failureHandler,
-  successHandler,
-  webhookHandler,
-};
-=======
   statusHandler,
   webhookHandler,
 };
->>>>>>> 51d92237903119989773bfccf662e0381c69085c
