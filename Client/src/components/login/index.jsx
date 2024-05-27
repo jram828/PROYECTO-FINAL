7a204a1bbe583823af1be10c155e0f51c54892e1
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-26 21:02:53 -0500

    IMPLEMENTACION LOGIN GITHUB

--- Client/src/components/login/index.jsx
+++ Client/src/components/login/index.jsx
@@ -1,35 +1,68 @@
-import {useState} from "react";
+import { useState } from "react";
 // import { validar } from "../../utils/validacion";
 import style from "./login.module.css";
 import { Link, useNavigate } from "react-router-dom";
 import { GoogleLogin } from "@react-oauth/google";
-import { useDispatch} from "react-redux";
 import { setAuth, setUserToken } from "../../redux/actions";
 import { jwtDecode } from "jwt-decode";
 import axios from "axios";
-
-
+import { useDispatch} from "react-redux";
+import { initializeApp } from "firebase/app";
+import { getAuth, GithubAuthProvider, signInWithPopup} from "firebase/auth";
+import  {loginWithProvider } from "../../redux/actions";
 
 // import { ClickHandlerCrear, ClickHandlerRecordatorio, Loginf } from "../../handlers/login";
 
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCgmLrDf4mM13hD6f47Yeq8KE7IvnwusC4",
+  authDomain: "legal-tech1111.firebaseapp.com",
+  projectId: "legal-tech1111",
+  storageBucket: "legal-tech1111.appspot.com",
+  messagingSenderId: "1037769772117",
+  appId: "1:1037769772117:web:919ee3a58c4809fafe18dc",
+  measurementId: "G-G0WCGL9H5B",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const githubProvider = new GithubAuthProvider();
+
 
 // eslint-disable-next-line react/prop-types
-const Login = ({ clickHandlerRecordatorio, clickHandlerCrear}) => {
+const Login = ({ clickHandlerRecordatorio, clickHandlerCrear }) => {
   const [userData, setUserData] = useState({
     email: "",
     password: "",
-    rol:""
+    rol: "",
   });
+
+
+const dispatch = useDispatch();
+ 
+  const handleSignIn = (provider) => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        const credentials = {
+          email: user.email,
+          displayName: user.displayName,
+          uid: user.uid,
+        };
+        dispatch(loginWithProvider(credentials));
+        navigate("/home");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+
 
   // const [errores, setErrores] = useState({
   //   cedula: "",
   //   password: "",
   // });
-
-
- 
-
-  const dispatch = useDispatch();
 
   const navigate = useNavigate();
   const handleChange = (e) => {
@@ -41,63 +74,59 @@ const Login = ({ clickHandlerRecordatorio, clickHandlerCrear}) => {
     });
   };
 
-  const submitHandler = async(e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
 
-  const { email, password, rol } = userData;
-  console.log("Datos login:", email, password,rol);
+    const { email, password, rol } = userData;
+    console.log("Datos login:", email, password, rol);
 
-  try {
-    const { data } = await axios(`/login/?email=${email}&password=${password}&rol=${rol}`);
-    console.log("Login 2:", data);
-    const { access } = data;
-    console.log('Access: ',access)
-    if (access) {
-      window.localStorage.setItem("loggedUser", JSON.stringify(data.usuario));
+    try {
+      const { data } = await axios(
+        `/login/?email=${email}&password=${password}&rol=${rol}`
+      );
+      console.log("Login 2:", data);
+      const { access } = data;
+      console.log("Access: ", access);
+      if (access) {
+        window.localStorage.setItem("loggedUser", JSON.stringify(data.usuario));
         dispatch(setAuth(access));
-      navigate("/home");
-
-
-
+        navigate("/home");
       } else {
         window.alert("Usuario o contraseña incorrectos");
       }
-  } catch (error) {
-    window.alert("Usuario o contraseña incorrectos");
-  }
+    } catch (error) {
+      window.alert("Usuario o contraseña incorrectos");
+    }
   };
 
   const ResponseMessage = async (response) => {
-   
     const user = jwtDecode(response.credential);
     // Loginf();
     dispatch(setUserToken(user));
-     const {rol } = userData;
-  console.log("Datos login:", user.email,rol);
-  try {
-    const { data } = await axios(`/login/google/?email=${user.email}&rol=&${rol}`);
-    
-    console.log("Login 3:", data);
-    // const { access } = data;
-    // if (user.email === data[0].correo) {
-        window.localStorage.setItem('loggedUser',JSON.stringify(data.usuario))
-        dispatch(setAuth(true));
-      navigate("/home");
+    const { rol } = userData;
+    console.log("Datos login:", user.email, rol);
+    try {
+      const { data } = await axios(
+        `/login/google/?email=${user.email}&rol=&${rol}`
+      );
 
+      console.log("Login 3:", data);
+      // const { access } = data;
+      // if (user.email === data[0].correo) {
+      window.localStorage.setItem("loggedUser", JSON.stringify(data.usuario));
+      dispatch(setAuth(true));
+      navigate("/home");
 
       // } else {
       //   window.alert("Usuario o contraseña incorrectos");
       // }
-     
-  } catch (error) {
-    window.alert("Usuario o contraseña incorrectos");
+    } catch (error) {
+      window.alert("Usuario o contraseña incorrectos");
     }
-    
   };
   const errorMessage = (error) => {
     console.log(error);
   };
-
 
   return (
     <div className="space-y-6 w-full max-w-lg p-6 bg-primary rounded-lg shadow-md">
@@ -201,6 +230,15 @@ const Login = ({ clickHandlerRecordatorio, clickHandlerCrear}) => {
       </form>
       <div className="flex justify-center">
         <GoogleLogin onSuccess={ResponseMessage} onError={errorMessage} />
+      </div>
+      <div className={style.github}>
+        <button
+          onClick={() => handleSignIn(githubProvider)}
+          className="btn btn-accent w-40"
+        >
+          Sign in with GitHub
+        </button>
+        {/* falta colocar que cuado te registres de mande a la home */}
       </div>
     </div>
   );
