23606eeb52e52840b92f4057fecc69fdec402453
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-28 00:10:27 -0500

    INCLUIR EMAIL COMPRADOR MERCADO PAGO

--- Server/src/controllers/pagos/crearOrden.js
+++ Server/src/controllers/pagos/crearOrden.js
@@ -29,15 +29,19 @@ const preference = new Preference(client);
             description: item.description,
             quantity: Number(item.quantity),
             unit_price: Number(item.unit_price),
-            id:item.id
+            id: item.id,
           },
         ],
+        payer: {
+          // email: req.body.payer,
+          email: "test_user_997292459@testuser.com",
+        },
+
         back_urls: {
           success:
             "https://proyecto-final-develop.vercel.app/#/home/payments/status",
           failure: "https://proyecto-final-develop.vercel.app/#/home",
-          pending:
-            "https://proyecto-final-develop.vercel.app/#/home",
+          pending: "https://proyecto-final-develop.vercel.app/#/home",
         },
         notification_url:
           "https://legaltech-develop.onrender.com/pagos/webhook",
@@ -50,49 +54,6 @@ const preference = new Preference(client);
     return error
 }
 
-
-  // // Step 2: Initialize the client object
-  // const client = new MercadoPagoConfig({
-  //   accessToken:
-  //     "TEST-3176577694700734-051711-d19831d5da8b20319a010655906a334c-1817941600",
-  //   options: { timeout: 5000, idempotencyKey: "PRIMERA PRUEBA" },
-  // });
-
-  // // Step 3: Initialize the API object
-  // const payment = new Payment(client);
-
-  // // Step 4: Create the request object
-  // const body = {
-  //   transaction_amount: 20,
-  //   description: "honorarios",
-  //   payment_method_id: "credit_card",
-  //   payer: {
-  //     email: "jram828@yahoo.com",
-  //   },
-  // };
-
-  // // Step 5: Create request options object - Optional
-  // const requestOptions = {
-  //   idempotencyKey: "primera prueba",
-  // };
-
-  // // Step 6: Make the request
-  // const newPayment=payment.create({ body, requestOptions }).then(console.log).catch(console.log);
-  // mercadopago.configure({
-  //   access_token:
-  //     "TEST-7845349164975835-051714-6919a564464a369582bcbc8ff6f8cc4f-1817941600",
-  // });
-
-  // const newOrden = await mercadopago.preferences.create({
-  //   item: [
-  //     {
-  //       title: "honorarios",
-  //       unit_price: 20,
-  //       currency_id: "COP",
-  //       quantity: 1,
-  //     },
-  //   ],
-  // });
 
 };
 
