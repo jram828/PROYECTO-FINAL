5990c0457f52af1183f8d26cb74886cbecb23100
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-31 13:43:30 -0500

    async status

--- Client/src/views/status/index.jsx
+++ Client/src/views/status/index.jsx
@@ -7,7 +7,7 @@ import Layout from "../../components/layout/layout";
 
 const ACCESSTOKEN = import.meta.env.VITE_ACCESS_TOKEN;
 
-function Status () {
+async function Status () {
   
   const [queries, setQueries] = useState({});
   const [datosPago, setDatosPago] = useState({});
@@ -54,7 +54,7 @@ function Status () {
 
   if (queries.payment_id) {
     // useEffect( () => {
-      const datos = obtenerPago(queries.payment_id);
+      const datos = await obtenerPago(queries.payment_id);
       console.log("Informacion del pago: ", datos);
       setDatosPago(datos);
     // }, []);
