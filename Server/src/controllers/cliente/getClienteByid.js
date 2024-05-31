49e4f1c63c5388d5c077e1de74799e3b1bfbcc02
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-31 08:03:15 -0500

    getClienteByid

--- Server/src/controllers/cliente/getClienteByid.js
+++ Server/src/controllers/cliente/getClienteByid.js
@@ -1,16 +1,20 @@
- import {models} from "../../DB.js";
+import {models} from '../../DB.js'
 
-const { Cliente } = models
-const getClienteById = async (cedulaCliente) => {
-  const cliente = await Cliente.findOne({
-    where: {
-      cedulaCliente,
-      activo: true,
-    },
-  });
-  return cliente;
-};
+ const { Cliente} = models;
+const getClienteById = async (cedulaCliente)=>{
+
+    const consulta= {
+        where: {
+            cedulaCliente:parseInt(cedulaCliente),
+            activo:true,
+        }
+    }
+    
+    const cliente = await Cliente.findOne(consulta);
+    if(!cliente) throw Error("Cliente no Registrado o no existe")
+    return cliente;
+}
 
 export {
-  getClienteById
-};
+    getClienteById,
+}
