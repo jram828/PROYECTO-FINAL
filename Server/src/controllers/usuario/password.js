989c69859c0beeb3df13eb14a4fa3b6a28fc3f8b
jram828 <128335805+jram828@users.noreply.github.com>
2024-06-02 06:50:08 -0500

    buscar password en abogado

--- Server/src/controllers/usuario/password.js
+++ Server/src/controllers/usuario/password.js
@@ -2,29 +2,30 @@ import { models } from "../../DB.js";
 import { sendEmailCliente } from "../../utils/emailNotifier.js";
 
 const { Cliente, Abogado } = models;
+
 const getPassword = async (email) => {
   console.log('Email get password: ',email)
-  const user = await Cliente.findOne({
+  const user = await Abogado.findOne({
     where: {
       correo: email,
     },
   });
   console.log("Password cliente: ", user.password);
-  if (!user) {
-    const user = await Abogado.findOne({
-      where: {
-        correo: email,
-      },
-    });
-    console.log("Password abogado: ", user.password);
-    if (!user) throw new Error("Usuario no encontrado");
-    sendEmailCliente(user.nombre, user.correo, "password",user.password);
-    return {
-      nombre: user.nombre,
-      correo: user.correo,
-      password: user.password,
-    };
-  }
+  // if (!user) {
+  //   const user = await Abogado.findOne({
+  //     where: {
+  //       correo: email,
+  //     },
+  //   });
+  //   console.log("Password abogado: ", user.password);
+  //   if (!user) throw new Error("Usuario no encontrado");
+  //   sendEmailCliente(user.nombre, user.correo, "password",user.password);
+  //   return {
+  //     nombre: user.nombre,
+  //     correo: user.correo,
+  //     password: user.password,
+  //   };
+  // }
   sendEmailCliente(user.nombre, user.correo, "password", user.password);
   return {
     nombre: user.nombre,
