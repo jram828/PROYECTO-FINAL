791c58f3e07201f72e006be3d373386a11a8bb9b
melizulian <melizulian@hotmail.com>
2024-06-03 22:58:19 -0300

    styles customers

--- Client/src/components/filtrosClientes/index.jsx
+++ Client/src/components/filtrosClientes/index.jsx
@@ -57,13 +57,16 @@ function FiltrosClientes() {
   <div className="md:flex md:flex-col md:space-y-4">
     <div>
       <div className="flex justify-end p-4">
-        <Link to="/home/customers/crearcliente" className="btn btn-md hover:bg-primary hover:text-white">Crear cliente</Link>
+        <Link to="/home/customers/crearcliente" className="btn btn-sm w-40 bg-accent text-white hover:bg-primary hover:text-white">
+          Crear cliente
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><path fill="white" d="M14 7H9V2H7v5H2v2h5v5h2V9h5z"></path></svg>
+          </Link>
       </div>
     </div>
-    <div className="flex flex-col gap-4 p-4 rounded-md max-h-screen bg-primary">
+    <div className="flex flex-col gap-4 p-4 rounded-md max-h-screen bg-white">
       <div className="flex items-center justify-between">
         <div className="flex flex-col gap-2">
-          <div className="max-w-xs"> 
+          <div className="flex gap-5"> 
             <OrderClientes />
             <SearchBar onFilter={handleFilter} />
             {filterApplied && <button className="btn" onClick={handleVerTodosClick}>Ver todos</button>}
--- Client/src/components/searchBarClientes/index.jsx
+++ Client/src/components/searchBarClientes/index.jsx
@@ -41,48 +41,55 @@ const SearchBar = ({ onFilter }) => {
   };
 
   return (
-    <div className="flex gap-2 mb-4">
-  <div className="flex gap-2 items-center">
-    <div className="input input-bordered flex items-center gap-2 mb-4">
-      <label>Nombre:</label>
-      <br />
+    <div className="flex gap-2 mb-4 ">
+  <div className="flex gap-2 items-center  max-w-full">
+    <div className="">
+      
       <input
-        placeholder="Busqueda..." 
+        placeholder="Nombre" 
         type="text" 
         value={nombre} 
         onKeyDown={handleKeyDown}
         onChange={(e) => handleInputChange(e, setNombre)}
-        className=""
+        className='input input-sm w-full max-w-40 !text-black'
       />
+      
     </div>
    
-    <div className="input input-bordered flex items-center gap-2 mb-4">
-      <label>Apellido:</label>
-      <br />
+    <div className="">
+      
       <input
-        placeholder="Busqueda..." 
+        placeholder="Apellido" 
         type="text" 
         value={apellido} 
         onKeyDown={handleKeyDown}
         onChange={(e) => handleInputChange(e, setApellido)}
-        className=""
+        className='input input-sm w-full max-w-40 !text-black'
       />
+      
     </div>
    
-    <div className="input input-bordered flex items-center gap-2 mb-4">
-      <label className=''>Ciudad:</label>
-      <br />
+    <div className="">
+      
       <input 
-        placeholder="Busqueda..." 
+        placeholder="Ciudad" 
         type="text" 
         value={ciudad} 
         onKeyDown={handleKeyDown}
         onChange={(e) => handleInputChange(e, setCiudad)}
-        className=""
+        className='input input-sm w-full max-w-40 !text-black'
       />
+      
     </div>
+    <div className="flex items-center gap-2">
+  
+  <button onClick={handleSearch} className="btn btn-sm w-40 border border-accent bg-white hover:bg-white">
+    Buscar
+    <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"><path fill="black" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"></path></svg>
+  </button>
+</div>
+
     
-    <button onClick={handleSearch} className='btn btn-md hover:bg-primary hover:text-white'>Buscar</button>
   </div>
 </div>
 
