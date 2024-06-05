fedfcd1e5fd0f95c815b9c91e50622f037d08a25
melizulian <melizulian@hotmail.com>
2024-06-05 15:03:19 -0300

    recuperar contrasena styles

--- Client/src/views/password/password.component.jsx
+++ Client/src/views/password/password.component.jsx
@@ -39,52 +39,55 @@ function Password() {
     };
   
   return (
-    <Layout>
-      <div className="contenedorrecordatorio">
-        <h1 className="titulo">Recordar contraseña</h1>
-        <br />
+    
+      <div className="flex items-center justify-center rounded-lg min-h-screen p-6 bg-white text-black">
+        
+        <div className="space-y-6  h-full p-6 bg-secondary rounded-lg shadow-md text-black">
+        <h1 className="text-2xl font-bold text-black text-center">Recordar contraseña</h1>
+        
 
         <form>
-          <br />
-          <br />
-          <br />
+        
+          
           <div className="recordar-password">
-            <label className="label-password" htmlFor="correo">
+            <label className="input input-sm !border-black input-secondary flex items-center max-w-xs !text-black" htmlFor="correo">
               Email:
-            </label>
             <input
               type="email"
               name="correo"
               id="email"
+              className="grow ml-2 text-black"
               onChange={handleChange}
             />
+            </label>
           </div>
-          <br />
-          <br />
-          <div className="recordar-password">
-            <input
-              className="btn btn-accent w-40"
+          
+          <div className="flex justify-between">
+          <Link to="/">
+            <button className="btn btn-sm w-35 border border-accent bg-white hover:bg-white">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 512 512"><path fill="none" stroke="black" strokeLinecap="round" strokeLinejoin="round" strokeWidth={50.5} d="M244 400L100 256l144-144M120 256h292"></path></svg>
+                  Volver
+                </button>
+            </Link>
+            <button
+              className="btn btn-sm w-40 bg-accent text-white hover:bg-primary hover:text-white"
               type="submit"
               name="Enviar"
               onClick={handlerPassword}
-            />
-            <br />
-            <br />
+            >
+              Enviar
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"><path fill="none" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 14L21 3m0 0l-6.5 18a.55.55 0 0 1-1 0L10 14l-7-3.5a.55.55 0 0 1 0-1z"></path></svg>
+            </button>
+          
 
-            <Link to="/">
-              <input
-                type="button"
-                name="volver"
-                value="volver"
-                className="btn btn-accent w-40"
-              />
-            </Link>
+            
           </div>
-          <br />
-          <br />
+          
         </form>
+        </div>
+        
       </div>
-    </Layout>
+    
   );
 }
 
