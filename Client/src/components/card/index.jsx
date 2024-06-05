83a04e9ccbc89578e64250aad145fe9c040bfab9
melizulian <melizulian@hotmail.com>
2024-06-05 09:04:13 -0300

    login landing calendar styles

--- Client/src/components/card/index.jsx
+++ Client/src/components/card/index.jsx
@@ -38,7 +38,6 @@ function Card({item}) {
       console.log('cedula', cedula);
 
   return (
-    
     <div className="w-64 mx-auto bg-secondary text-black shadow-lg rounded-lg overflow-hidden h-64">
       <div className="avatar flex justify-center mt-4">
         <img src={imagen} alt="Profile Picture" className="rounded-full !w-28 !h-28" />
