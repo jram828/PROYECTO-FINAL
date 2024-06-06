d0b40c3137fec8a5d61f00b65c39f64c0abca0ab
melizulian <melizulian@hotmail.com>
2024-06-06 10:32:29 -0300

    lawyers y cases views styles fix

--- Client/src/components/filtrosCasos/filtrosCasos.jsx
+++ Client/src/components/filtrosCasos/filtrosCasos.jsx
@@ -49,49 +49,48 @@ function FiltrosCasos() {
   console.log("usercasos", userCasos)
 
   return (
-    <div className="container md:flex md:flex-col md:space-y-4">
-  <div className="flex justify-end p-4">
-    {user.administrador === true || user.cedulaAbogado ? (
-      <Link to='/home/cases/crearcaso' className='btn btn-sm w-40 bg-accent text-white hover:bg-primary hover:text-white'>
-        Crear caso
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><path fill="white" d="M14 7H9V2H7v5H2v2h5v5h2V9h5z"></path></svg>
-      </Link>
-    ) : null}
-  </div>
+    <div className="flex w-full">
+      <div className="md:flex md:flex-col md:space-y-4 w-full">
 
-  <div className="flex flex-col gap-4 p-4 rounded-md max-h-screen bg-white">
-    <div className="flex items-center justify-between">
-      <div className="flex gap-3">
-        <div>
-          <OrderCasos />
+      <div className="flex justify-end p-2">
+          {user.administrador === true || user.cedulaAbogado ? (
+            <Link to='/home/cases/crearcaso' className='btn btn-sm w-40 bg-accent text-white hover:bg-primary hover:text-white'>
+              Crear caso
+              <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><path fill="white" d="M14 7H9V2H7v5H2v2h5v5h2V9h5z"></path></svg>
+            </Link>
+          ) : null}
         </div>
-        <div>
-          <SearchBar onFilter={handleFilter} />
-        </div>
-        {filterApplied && (
-          <button onClick={handleVerTodosClick} className="btn btn-sm bg-accent text-white hover:bg-primary hover:text-white">
-            Ver todos
-          </button>
-        )}
-      </div>
-    </div>
-  </div>
 
-  <div className="md:flex md:flex-row md:space-x-4">
-    <div className="p-2">
-      <div className="flex flex-col gap-4">
-        {isLoading ? (
-          <div className="loading-container">
-            <h2 className="loading">Cargando...</h2>
-            <img className="loading-image" src={loading} alt="loading" />
+          <div className="flex justify-between self-center">
+            <div className="flex gap-4">
+                <OrderCasos />
+                <SearchBar onFilter={handleFilter} />
+              {filterApplied && (
+                <button onClick={handleVerTodosClick} className="btn btn-sm bg-accent text-white hover:bg-primary hover:text-white">
+                  Ver todos
+                </button>
+              )}
+            </div>
           </div>
-        ) : (
-          <TarjetasCasos casos={userCasos} />
-        )}
-      </div>
+        
+
+        
+          <div className="p-2">
+            <div className="flex flex-col">
+              {isLoading ? (
+                <div className="loading-container">
+                  <h2 className="loading">Cargando...</h2>
+                  <img className="loading-image" src={loading} alt="loading" />
+                </div>
+              ) : (
+                <TarjetasCasos casos={userCasos} />
+              )}
+            </div>
+          </div>
+
+        </div>
+      
     </div>
-  </div>
-</div>
 
   
   );
